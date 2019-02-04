7aad2f9a379daf202354b1c253a8b48b00d53837
iainkirkpatrick <kirkpatrick.iain@gmail.com>
2019-02-04 13:19:44 +1300

    get basic simple-peer example working in react

--- client/src/App.js
+++ client/src/App.js
@@ -4,19 +4,57 @@ import Peer from 'simple-peer'
 import './App.css';
 
 class App extends Component {
-  startBroadcasting () {
-    const bPeer = new Peer({ initiator: true })
+  constructor () {
+    super()
+    this.state = {
+      peer: null,
+      incoming: '',
+      outgoing: ''
+    }
+  }
+
+  componentDidMount () {
+    const p = new Peer({ initiator: window.location.hash === '#1', trickle: false })
+    p.on('error', function (err) { console.log('error', err) })
+    p.on('signal', (data) => {
+      console.log('SIGNAL', JSON.stringify(data))
+      this.setState({
+        outgoing: JSON.stringify(data)
+      })
+    })
+
+    p.on('connect', function () {
+      console.log('CONNECT')
+      p.send('whatever' + Math.random())
+    })
+
+    p.on('data', function (data) {
+      console.log('data: ' + data)
+    })
+
+    this.setState({
+      peer: p
+    })
+  }
+
+  handleSubmit = () => {
+    this.state.peer.signal(JSON.parse(this.state.incoming))
+  }
+
+  updateIncoming = (ev) => {
+    this.setState({
+      incoming: ev.target.value
+    })
   }
 
   render() {
     return (
       <div className="App">
         <div>
-          <button>start broadcasting</button>
+          <textarea value={this.state.incoming} onChange={this.updateIncoming}></textarea>
+          <button onClick={this.handleSubmit}>submit</button>
         </div>
-        <div>
-          <h1>BROADCASTERS</h1>
-        </div>
+        <pre id="outgoing"></pre>
       </div>
     );
   }
